c6b396105409d3abca9c593fe2df02c4816815d6
WIsavachart <wisavachart.s@gmail.com>
2024-08-23 07:23:47 +0700

    First create

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::factory(1000)->create();
+        $this->call(EventSeeder::class);
+        $this->call(AttendeeSeeder::class);
+    }
+}
